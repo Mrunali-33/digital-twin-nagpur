7be52a8e936a30dc226211158bc694546ab75dec
Mrunali Chinchkhede <mrunalichinchkhede2@gmail.com>
2026-05-19 13:31:33 +0530

    Added latest-data API endpoint

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,29 @@
+import "../styles/sidebar.css";
+
+function Sidebar() {
+
+  return (
+
+    <div className="sidebar">
+
+      <h2>🌍 Digital Twin</h2>
+
+      <ul>
+
+        <li>📡 Live Monitoring</li>
+
+        <li>🌫 AQI Tracking</li>
+
+        <li>🌊 Flood Risk</li>
+
+        <li>📈 Analytics</li>
+
+        <li>🚨 Alerts</li>
+
+      </ul>
+
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ frontend/src/data/zoneData.js
@@ -0,0 +1,31 @@
+export const zones = [
+
+  {
+    id: 1,
+    name: "NEERI Core",
+    position: [21.1235, 79.0472],
+    risk: "LOW"
+  },
+
+  {
+    id: 2,
+    name: "Rahate Colony A",
+    position: [21.1245, 79.0485],
+    risk: "MODERATE"
+  },
+
+  {
+    id: 3,
+    name: "Rahate Colony B",
+    position: [21.1252, 79.0492],
+    risk: "HIGH"
+  },
+
+  {
+    id: 4,
+    name: "Rahate Colony C",
+    position: [21.1229, 79.0465],
+    risk: "LOW"
+  }
+
+];
