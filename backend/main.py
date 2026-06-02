03c78a3a1fa86ec5a26b84b88741448f1137c5fe
Mrunali Chinchkhede <mrunalichinchkhede2@gmail.com>
2026-06-02 15:31:14 +0530

    Added backend processing files

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,14 @@
-from processing import process_environmental_data
 from fastapi import FastAPI
 from pydantic import BaseModel
-
 from fastapi.middleware.cors import CORSMiddleware
 
-# Create FastAPI app
+from processing import process_environmental_data
+
+# CREATE FASTAPI APP
+
 app = FastAPI()
 
-latest_data = {}
+# ENABLE CORS
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,38 +18,47 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Store latest data
+# STORE LATEST ENVIRONMENTAL DATA
+
 latest_data = {}
 
-# Sensor Data Model
+# SENSOR DATA MODEL
+
 class SensorData(BaseModel):
+
     zone: str
     aqi: int
     rain: int
     water_level: int
 
-# Home Route
+# HOME ROUTE
+
 @app.get("/")
 def home():
+
     return {
         "message": "Integrated Environmental Digital Twin Backend Running"
     }
 
-# Sensor Data API
+# SENSOR DATA API
+
 @app.post("/sensor-data")
 def receive_sensor_data(data: SensorData):
 
     raw_data = data.dict()
 
-    # Process environmental data
+    # PROCESS ENVIRONMENTAL DATA
+
     processed_data = process_environmental_data(raw_data)
 
     global latest_data
+
     latest_data = processed_data
 
     print("\n===== PROCESSED ENVIRONMENT DATA =====")
 
     for key, value in processed_data.items():
+
         print(f"{key}: {value}")
 
     return {
@@ -56,16 +66,9 @@ def receive_sensor_data(data: SensorData):
         "processed_data": processed_data
     }
 
-    for key, value in processed_data.items():
-        print(f"{key}: {value}")
+# GET LATEST LIVE DATA
 
-    return {
-        "status": "success",
-        "processed_data": processed_data
-    }
+@app.get("/latest-data")
+def get_latest_data():
 
-
-    @app.get("/latest-data")
-    def get_latest_data():
-
-        return latest_data
+    return latest_data
